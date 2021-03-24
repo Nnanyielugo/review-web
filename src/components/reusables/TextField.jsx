e42e14a0ea708c81f3f29a57ff1da3d9df12078c
Nnanyielugo <kz.onyegbule@gmail.com>
2021-03-24 20:51:43 +0100

    handle final form prop changes

--- src/components/reusables/TextField.jsx
+++ src/components/reusables/TextField.jsx
@@ -3,24 +3,49 @@ import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 
 export default function ComposedTextField({
-  label,
-  inputType,
-  fullWidth,
-  className,
+  label, fullWidth, className, inputtype,
+  input,
+  input: {
+    onChange, onFocus, onBlur, value,
+  },
+  meta,
+  meta: {
+    touched, invalid, error,
+  },
 }) {
+  const handleChange = (evt) => {
+    const vals = evt.target.value;
+    onChange(vals);
+  };
   return (
     <TextField
       label={label}
-      type={inputType}
+      type={inputtype}
       fullWidth={fullWidth}
       className={className}
+      error={
+        (meta && invalid && touched)
+          || (meta && touched && error)
+      }
+      value={input && value}
+      onChange={handleChange}
+      onBlur={input && onBlur}
+      onFocus={input && onFocus}
     />
   );
 }
 
 ComposedTextField.propTypes = {
   label: PropTypes.string.isRequired,
-  inputType: PropTypes.string,
+  inputtype: PropTypes.string,
   fullWidth: PropTypes.bool,
   className: PropTypes.node,
+  error: PropTypes.bool,
+  input: PropTypes.object,
+  meta: PropTypes.object,
+};
+
+ComposedTextField.defaultProps = {
+  error: false,
+  fullWidth: false,
 };
