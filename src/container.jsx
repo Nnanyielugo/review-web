acdf1af52752e7345b879f297c3913ed94a93639
Nnanyielugo <kz.onyegbule@gmail.com>
2021-03-24 20:45:07 +0100

    move auth check and fetch reviews to container component

--- src/container.jsx
+++ src/container.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import Navigation from 'components/navigation';
+import Navigation from '_components/navigation';
+import { useAuth } from '_providers/Auth';
+import { useReview } from '_providers/Review';
 import ModalManager from './ModalManager';
 import ThemeProvider from './ThemeProvider';
 
@@ -14,6 +16,16 @@ const useStyles = makeStyles(() => ({
 
 export default function Index({ Component }) {
   const classes = useStyles();
+  const { autoAuth } = useAuth();
+  const { fetchReviews } = useReview();
+
+  useEffect(async () => {
+    autoAuth();
+    await Promise.allSettled([
+      fetchReviews(),
+    ]);
+  }, []);
+
   return (
     <ThemeProvider>
       <main className={classes.root}>
