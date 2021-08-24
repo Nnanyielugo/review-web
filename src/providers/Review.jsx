bc4c3a1af3b8bd175661dc629fd0025304d0a6b0
Nnanyielugo <kz.onyegbule@gmail.com>
2021-08-24 06:36:04 +0100

    fix provider update bug

--- src/providers/Review.jsx
+++ src/providers/Review.jsx
@@ -1,16 +1,17 @@
-import React, {
-  useReducer, useContext, createContext,
-} from 'react';
+import React, { useReducer, useContext, createContext } from 'react';
 import PropTypes from 'prop-types';
 import FetchHelper from '_utils/fetch-utils';
 
 const FETCH_REVIEWS = 'FETCH_REVIEWS';
 const FETCH_REVIEWS_FAILED = 'FETCH_REVIEWS_FAILED';
+const FETCH_REVIEW = 'FETCH_REVIEW';
+const FETCH_REVIEW_FAILED = 'FETCH_REVIEW_FAILED';
 
 const initialState = {
   reviews: [],
   reviewsCount: 0,
   error: null,
+  review: null,
 };
 
 function reducer(state, action) {
@@ -21,8 +22,19 @@ function reducer(state, action) {
         ...state,
         reviews: payload.reviews,
         reviewsCount: payload.reviewsCount,
+        review: null,
       };
     case FETCH_REVIEWS_FAILED:
+      return {
+        ...state,
+        error: `There was an error executing your request: ${payload.error}`,
+      };
+    case FETCH_REVIEW:
+      return {
+        ...state,
+        review: payload.review,
+      };
+    case FETCH_REVIEW_FAILED:
       return {
         ...state,
         error: `There was an error executing your request: ${payload.error}`,
@@ -53,16 +65,30 @@ export default function Provider({ children }) {
     }
   };
 
+  const fetchReview = async (id) => {
+    try {
+      const response = await FetchHelper(`/api/reviews/${id}`);
+      dispatch({
+        type: FETCH_REVIEW,
+        payload: response,
+      });
+      return response;
+    } catch (err) {
+      dispatch({
+        type: FETCH_REVIEW_FAILED,
+        payload: err.message,
+      });
+      throw err;
+    }
+  };
+
   const state = {
     reviews,
     fetchReviews,
+    fetchReview,
   };
 
-  return (
-    <Context.Provider value={state}>
-      {children}
-    </Context.Provider>
-  );
+  return <Context.Provider value={state}>{children}</Context.Provider>;
 }
 
 Provider.propTypes = {
