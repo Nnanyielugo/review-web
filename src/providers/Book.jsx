146b2deeca3c5699f2c8f42fe4b52df02dc82409
Nnanyielugo <kz.onyegbule@gmail.com>
2021-08-30 23:29:20 +0100

    init book list

---
+++ src/providers/Book.jsx
@@ -0,0 +1,68 @@
+import React, { useReducer, useContext, createContext } from 'react';
+import PropTypes from 'prop-types';
+import FetchHelper from '_utils/fetch-utils';
+
+const FETCH_REVIEWS = 'FETCH_REVIEWS';
+const FETCH_REVIEWS_FAILED = 'FETCH_REVIEWS_FAILED';
+
+const initialState = {
+  books: [],
+  error: null,
+  review: null,
+};
+
+function reducer(state, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case FETCH_REVIEWS:
+      return {
+        ...state,
+        books: payload.books,
+      };
+    case FETCH_REVIEWS_FAILED:
+      return {
+        ...state,
+        error: `There was an error executing your request: ${payload.error}`,
+      };
+    default:
+      return state;
+  }
+}
+
+export const Context = createContext(null);
+
+export default function Provider({ children }) {
+  const [books, dispatch] = useReducer(reducer, initialState);
+
+  const fetchBooks = async () => {
+    try {
+      const response = await FetchHelper('/api/books/');
+      dispatch({
+        type: FETCH_REVIEWS,
+        payload: response,
+      });
+      return response;
+    } catch (err) {
+      dispatch({
+        type: FETCH_REVIEWS_FAILED,
+        payload: err.message,
+      });
+      throw err;
+    }
+  };
+
+  const state = {
+    books,
+    fetchBooks,
+  };
+
+  return <Context.Provider value={state}>{children}</Context.Provider>;
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export function useBook() {
+  return useContext(Context);
+}
